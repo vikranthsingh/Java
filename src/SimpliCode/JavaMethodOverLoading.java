051142daad64e7a2977aa1bd25686dd36e1856ff
vikra <vikranth.civilenginer@gmail.com>
2022-04-05 23:35:41 +0530

    update on Streams

--- src/SimpliCode/JavaMethodOverLoading.java
+++ src/SimpliCode/JavaMethodOverLoading.java
@@ -1,21 +1,24 @@
 package SimpliCode;
 
 class Multiply {
-    int mul(int x, int y) {
+    int calculation(int x, int y) {
+        return x + y;
+    }
+
+    int calculation(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    double calculation(double x, double y) {
         return x * y;
-    }
-    int mul(int x, int y, int z) {
-        return x * y *z;
-    }
-    double mul(int x, double y, double z) {
-        return x * y *z;
     }
 }
 
 public class JavaMethodOverLoading {
     public static void main(String[] args) {
-        Multiply multiply = new Multiply();
-        double r = multiply.mul(1, 2, 3.1);
-        System.out.println(r);
+        Multiply cal = new Multiply();
+        System.out.println(cal.calculation(3,3));
+        System.out.println(cal.calculation(3,4,5));
+        System.out.println(cal.calculation(3.4,4.3));
     }
 }
--- src/SimpliCode/JavaMethodOverRiding.java
+++ src/SimpliCode/JavaMethodOverRiding.java
@@ -1,29 +1,21 @@
 package SimpliCode;
 
-class Parent3 {
-    static int age = 28;
-    public static void son() {
-        System.out.println("Get married");
-    }
-    public void say(){
-        System.out.println("Saying");
+class Vehicle{
+    public static final String NAME = "KTM";
+    void name(int speed){
+        System.out.println("Hero Honda");
     }
 }
-
-class Child3 extends Parent3 {
+class MotorBike extends Vehicle{
     @Override
-    public void say() {
-        System.out.println("Don't want to Speak");
+    void name(int speed) {
+        System.out.println(speed-10);
     }
 }
-
 public class JavaMethodOverRiding {
     public static void main(String[] args) {
-        Parent3.son();                      //Static method is called by ClassName.StaticMethodName
-        System.out.println(Parent3.age);    //Static variable is called by ClassName.StaticVariableName
-
-
-        Child3 child = new Child3();
-        child.say();    //Instance method because it belongs to Object
+       MotorBike bike = new MotorBike();
+       bike.name(100);
+        System.out.println(Vehicle.NAME);
     }
 }
